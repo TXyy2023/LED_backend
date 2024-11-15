523a72af235ed32dab60c3e0cf299b17e5acc30d
1234 <a1234@r.local>
2024-11-15 17:18:42 +0800

    fix bug

--- font_convert.py
+++ font_convert.py
@@ -2,11 +2,13 @@ from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
 def generate_font_bitmap(chars, font_path='proggy_square/ProggySquare.ttf', size=16):
-    # 字典存储每个字符的字模
-    bitmaps = {}
-    # 加载字体
-    font = ImageFont.truetype(font_path, size*2)
+    bit_list_hex=[]
     for char in chars:
+        # 字典存储每个字符的字模
+        bitmaps = {}
+        # 加载字体
+        font = ImageFont.truetype(font_path, size*2)
+        
         # 创建空白图片
         img = Image.new('1', (size, size), color=0)
         draw = ImageDraw.Draw(img)
@@ -20,34 +22,34 @@ def generate_font_bitmap(chars, font_path='proggy_square/ProggySquare.ttf', size
         # 转换为二进制数组并存储
         bitmap = np.array(img)
         bitmaps[char] = bitmap
-    # print(bitmap)
-    bit_list_16=[]
-    for char, bitmap in bitmaps.items():
-        print(f"Character: {char}")
-        for row in bitmap:
-            row_data=''.join(['1' if pixel else '0' for pixel in row])
-            # print(row_data)
-            bit_list_16.append(row_data)
-            print(''.join(['1' if pixel else '0' for pixel in row]))
-        # print("\n")
-   
-    bit_list_16 = [''.join(row) for row in zip(*bit_list_16)]
-    print(bit_list_16)
-    # bit_list_8=[]
-    # for i in bit_list_16:
-    #     first_8_bits = i[:8]
-    #     second_8_bits = i[8:]
-    #     fir_hex=hex(int(first_8_bits, 2))[2:].zfill(2)
-    #     sec_hex=hex(int(second_8_bits, 2))[2:].zfill(2)
-    #     bit_list_8.append(fir_hex)
-    #     bit_list_8.append(sec_hex)
-    bit_list_hex=[]
-    for i in bit_list_16:
-        # row_data=hex(int(i, 2))[2:].zfill(4)
-        row_data = hex(int(i, 2))[2:]
-        print(row_data)
-        bit_list_hex.append(row_data)
-    return row_data
+        # print(bitmap)
+        bit_list_16=[]
+        for char, bitmap in bitmaps.items():
+            print(f"Character: {char}")
+            for row in bitmap:
+                row_data=''.join(['1' if pixel else '0' for pixel in row])
+                # print(row_data)
+                bit_list_16.append(row_data)
+                print(''.join(['1' if pixel else '0' for pixel in row]))
+            # print("\n")
+        print(bit_list_16) 
+        bit_list_16 = [''.join(row) for row in zip(*bit_list_16)]
+        # print(bit_list_16)
+        # bit_list_8=[]
+        # for i in bit_list_16:
+        #     first_8_bits = i[:8]
+        #     second_8_bits = i[8:]
+        #     fir_hex=hex(int(first_8_bits, 2))[2:].zfill(2)
+        #     sec_hex=hex(int(second_8_bits, 2))[2:].zfill(2)
+        #     bit_list_8.append(fir_hex)
+        #     bit_list_8.append(sec_hex)
+        
+        for i in bit_list_16:
+            # row_data=hex(int(i, 2))[2:].zfill(4)
+            row_data = hex(int(i, 2))[2:]
+            print(row_data)
+            bit_list_hex.append(row_data)
+    return bit_list_hex
     # return bit_list_8
 
 def image_to_bitmap(img_path, size=(16, 16)):
@@ -81,7 +83,8 @@ def image_to_bitmap(img_path, size=(16, 16)):
     return bit_list_8
 if __name__ == '__main__':
     # 测试输出
-    chars = "A"  # 多个字符
+    chars = "BUCT"  # 多个字符
+
     bit_list_8 = generate_font_bitmap(chars)
     print(bit_list_8)
 
